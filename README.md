306ce501865be858daa4a27456fc15c4ad2f9d20
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-08 14:15:10 -0800

    Finished code using TODOs in week 3, preparing to build up for week-4 starting code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Week 3:
+# Week 4:
 
-## This week, we will continue building out our application - and learning to make API calls!
+## This week, we will continue building out our application while learning how to use a SQL database and SQLAlchemy ORM!
 
-Follow the TODOs in [assignments](https://github.com/flask-django-independent-study/rookie/blob/master/Assignments/Week-3-APIs.md)!
+Follow the TODOs in [assignments](https://github.com/flask-django-independent-study/rookie/blob/master/Assignments/Week-4-SQL-SQLAlchemy.md)!
 
-Don't forget to check out the stretch challenges for extra practice! 
+Don't forget to check out the stretch challenges for extra practice!
--- app.py
+++ app.py
@@ -6,37 +6,86 @@ This is a doc string! It's a special kind of comment that is expected
 in Python files. Usually, you use this at the top of your code and in
 every function & class to explain what the code does.
 """
+import os
+import requests
 from flask import Flask, request, render_template, url_for
 from guest import Guest
 from datetime import datetime, date
+from pprint import PrettyPrinter
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("API_KEY")
 
 # Define global variables (stored here for now)
 
 guest_list = []
 
+# This initializes our PrettyPrinter object:
 
-@app.route('/')
+pp = PrettyPrinter(indent=4)
+
+today = date.today()
+# Now, let's just get the month as a number:
+month = today.strftime("%m")
+# Now, let's get the current year:
+year = today.strftime("%Y")
+
+
+##########################################
+#           # Helper functions           #
+##########################################
+
+
+def get_holiday_data(result):
+    """Loop through our JSON results and get only the information we need."""
+    data = []
+    for holiday in result["response"]["holidays"]:
+        new_holiday = {
+            "name": holiday["name"],
+            "description": holiday["description"],
+            "date": holiday["date"]["iso"],
+        }
+        data.append(new_holiday)
+    return data
+
+
+##########################################
+#           Routes                       #
+##########################################
+
+
+@app.route("/")
 def homepage():
     """Return template for home."""
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/about')
+@app.route("/about")
 def about_page():
-    """Show user party information."""
-    # Sometimes, a cleaner way to pass variables to templates is to create a
-    # context dictionary, and then pass the data in by dictionary key
+    """Show user event information."""
+    url = "https://calendarific.com/api/v2/holidays"
 
-    context = {
-        "date": "10/31/2020",
-        "time": "10:00 pm"
+    params = {
+        "api_key": API_KEY,
+        "country": "US",
+        "year": year,
+        "month": month,
     }
-    return render_template('about.html', **context)
+
+    result_json = requests.get(url, params=params).json()
+    # You can use pp.pprint() to print out your result -
+    # This will help you know how to access different items if you get stuck
+    # pp.pprint(result_json)
+
+    # Call get_holiday_data to return our list item
+
+    holidays = get_holiday_data(result_json)
+
+    return render_template("about.html", holidays=holidays)
 
 
-@app.route('/guests', methods=['GET', 'POST'])
+@app.route("/guests", methods=["GET", "POST"])
 def show_guests():
     """
     Show guests that have RSVP'd.
@@ -54,10 +103,10 @@ def show_guests():
         return render_template("guests.html", guests=guest_list)
 
 
-@app.route('/rsvp')
+@app.route("/rsvp")
 def rsvp_guest():
     """Show form for guests to RSVP for events."""
-    return render_template('rsvp.html')
+    return render_template("rsvp.html")
 
 
 if __name__ == "__main__":

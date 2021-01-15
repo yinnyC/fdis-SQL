5739438c8b4891035e6a527a92f65c7db6374288
Yin <yinnnn17@gmail.com>
2021-01-14 22:04:45 -0800

    finished instruction 9

--- events_app/__init__.py
+++ events_app/__init__.py
@@ -1,7 +1,9 @@
 """Import packages and modules for initializing our app."""
+from events_app.main.routes import main
 from flask import Flask
 
 # TODO: import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from events_app.config import Config
 
 # Some of this undoubtedly looks a little foreign right now.
@@ -10,14 +12,11 @@ from events_app.config import Config
 
 app = Flask(__name__)
 # TODO: add your config statement so you can access environment variables!
-
+app.config.from_object(Config)
 
 # TODO: Use the instructions in your assignment
 # to initialize your database taking our app as its parameter.
-
-
-from events_app.main.routes import main
-
+db = SQLAlchemy(app)
 app.register_blueprint(main)
 
 # TODO: add your statement to create database tables

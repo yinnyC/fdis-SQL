e1176bee5e745e28281c4c5d0ba51f92b1e58d71
Yin <yinnnn17@gmail.com>
2021-01-16 23:21:17 -0800

    finished step 14

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ every function & class to explain what the code does.
 from events_app import app
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
